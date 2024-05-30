f5635555694623fd55d2e915c3f6681c56fe7be2
MuhamedMagdy97 <Mohamed.Magdy1997@outlook.com>
2024-05-30 06:14:20 +0300

    Fixing 404 error that occor in GH

--- src/index.js
+++ src/index.js
@@ -26,4 +26,13 @@ root.render(
   </CartContextProvider>
 );
 
+// this will prevent the 404 page in gh
+<script type="text/javascript">
+  var pathSegmentsToKeep = 1; var l = window.location; l.replace( l.protocol +
+  "//" + l.hostname + (l.port ? ":" + l.port : "") + l.pathname .split("/")
+  .slice(0, 1 + pathSegmentsToKeep) .join("/") + "/?/" + l.pathname .slice(1)
+  .split("/") .slice(pathSegmentsToKeep) .join("/") .replace(/&/g, "~and~") +
+  (l.search ? "&" + l.search.slice(1).replace(/&/g, "~and~") : "") + l.hash );
+</script>;
+
 reportWebVitals();
