d2a54e546ec033e4d6c8631acd20b598147a593a
Mohamed Magdy <106457573+MuhamedMagdy97@users.noreply.github.com>
2026-08-16 22:38:07 +0300

    Improve missing-page feedback

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import style from "./NotFound.module.css";
+import { Link } from "react-router-dom";
+import errorImage from "../../Assets/images/error.svg";
 
 export default function NotFound() {
-  return (
-    <>
-      <h2>NotFound</h2>
-    </>
-  );
+  return <section className="text-center py-5"><img src={errorImage} alt="Page not found" className="img-fluid" style={{ maxWidth: "20rem" }} /><h1 className="h2 mt-3">Page not found</h1><p>The page you requested does not exist or has moved.</p><Link to="/" className="btn bg-main text-light">Back to home</Link></section>;
 }
