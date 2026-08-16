d0524d34df36e201d665b4951faf1b116c527492
Mohamed Magdy <106457573+MuhamedMagdy97@users.noreply.github.com>
2026-08-16 22:33:12 +0300

    Configure application providers

--- src/index.js
+++ src/index.js
@@ -2,37 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App.jsx";
 import reportWebVitals from "./reportWebVitals";
 import UserContextProvider from "./Context/UserContext.js";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 import CartContextProvider from "./Context/CartContext.js";
 
-let queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+      staleTime: 60000,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <CartContextProvider>
+  <QueryClientProvider client={queryClient}>
     <UserContextProvider>
-      <QueryClientProvider client={queryClient}>
+      <CartContextProvider>
         <App />
-        {/* <ReactQueryDevtools /> */}
-      </QueryClientProvider>
+      </CartContextProvider>
     </UserContextProvider>
-  </CartContextProvider>
+  </QueryClientProvider>
 );
-
-// this will prevent the 404 page in gh
-<script type="text/javascript">
-  var pathSegmentsToKeep = 1; var l = window.location; l.replace( l.protocol +
-  "//" + l.hostname + (l.port ? ":" + l.port : "") + l.pathname .split("/")
-  .slice(0, 1 + pathSegmentsToKeep) .join("/") + "/?/" + l.pathname .slice(1)
-  .split("/") .slice(pathSegmentsToKeep) .join("/") .replace(/&/g, "~and~") +
-  (l.search ? "&" + l.search.slice(1).replace(/&/g, "~and~") : "") + l.hash );
-</script>;
 
 reportWebVitals();
